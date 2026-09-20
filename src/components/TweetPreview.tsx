5246357a42d7ad15b5f8a014cf2823b3be74969c
Bangoura Aboubacar <babougoura03@gmail.com>
2026-09-20 21:58:49 +0200

    Ajout du voir plus / voir moins

--- src/components/TweetPreview.tsx
+++ src/components/TweetPreview.tsx
@@ -1,19 +1,29 @@
 import type { JSX } from "react/jsx-runtime";
 import type { Tweet } from "../types/Tweet";
 import "./TweetPreview.css";
-
+import { useState } from "react";
+const max_length = 180;
 export type TweetPreviewProps = {
     tweet : Tweet
 }
 
 export const TweetPreview = ({tweet}: TweetPreviewProps) : JSX.Element =>{
+    const [isExpanded, setIsExpanded] =useState(false);
+    const islong = tweet.content.length> max_length;
+    const afficheContent = 
+        islong && !isExpanded
+        ? tweet.content.slice(0, max_length) +"..." :
+        tweet.content;
+    
     return (
         <div>
             <h1>{tweet.authorName}</h1>
             <h2>@{tweet.authorHandle}</h2>
             <h3>{new Date(tweet.createdAt).toLocaleDateString("fr-FR")}</h3>
             {tweet.image && <img className="tweet-image" src={tweet.image.url} alt={tweet.image.alt}/>}
-            <p>{tweet.content}</p>
+            <p>{afficheContent}</p>
+            {islong && <button type="button" onClick={() => setIsExpanded(e => !e)}>
+                {isExpanded ? "Voir moins" : "Voir plus"}</button>}
         </div>
     )
 }
--- src/data/tweets.ts
+++ src/data/tweets.ts
@@ -18,7 +18,7 @@ export const tweets : Array<Tweet> = [{
     authorHandle: "mathematicienne",
     content: "La machine analytique tisse des motifs algébriques comme le métier à tisser Jacquard tisse des fleurs et des feuilles",
     image: {
-        url: "• https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/500px-Ada_Lovelace_portrait.jpg",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/500px-Ada_Lovelace_portrait.jpg",
         alt: "Photo de la mathematicienne Ada"
     },
     createdAt: "2026-08-01T09:12:00.000Z"
@@ -26,13 +26,13 @@ export const tweets : Array<Tweet> = [{
     id : "00000000-0000-0000-0000-000000000004",
     authorName : "Vegeta",
     authorHandle: "PrinceSaiyan",
-    content: "Même si je dois rester seul dans les ténèbres, je continuerai d'avancer. Je ne me bats pas pour être le meilleur… je me bats pour ne plus jamais être faible.",
+    content: "Je préfère mourir plutôt que de continuer à vivre dans l’ombre de quelqu’un d’autre. Je suis le prince des Saiyans, et je me battrai jusqu’au bout pour dépasser mes propres limites et prouver ma valeur.",
     createdAt: "2026-07-01T09:13:50.000Z"
 },{
     id : "00000000-0000-0000-0000-000000000005",
     authorName : "Saitama",
     authorHandle: "OnePunchMan",
-    content: "Si tu ne peux pas gagner, ne meurs pas. Si tu meurs, tu ne pourras jamais gagner.",
+    content: "Peu importe la difficulté de ton entraînement ou le nombre de fois où tu échoues, si tu continues à avancer sans abandonner, tu finiras forcément par devenir plus fort qu’hier.",
     createdAt: "2026-10-01T09:12:00.000Z"
 },{
     id : "00000000-0000-0000-0000-000000000006",
@@ -60,7 +60,7 @@ export const tweets : Array<Tweet> = [{
     id : "00000000-0000-0000-0000-000000000009",
     authorName : "Eisei",
     authorHandle: "KingOfChine",
-    content: "Je deviendrai le roi qui mettra fin à toutes les guerres",
+    content: "Je ne veux pas simplement gagner des batailles. Je veux mettre fin au cycle de la guerre et construire un monde dans lequel les générations futures n’auront plus à vivre dans la peur de la guerre.",
     createdAt: "2026-11-01T10:12:00.000Z"
 },{
     id : "00000000-0000-0000-0000-000000000010",
